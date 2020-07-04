e0d3406f5e336c0776145b08971636cf438e49ef
NectDz <54990613+NectDz@users.noreply.github.com>
2020-07-04 18:15:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ Highcharts.getJSON(
         },
 
         title: {
-          text: "Best Coronacation Sites 2020"
+          text: "Safest Vacation Sites in the USA 2020""
         },
 
         legend: {
